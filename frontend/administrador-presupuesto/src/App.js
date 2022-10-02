0493a8458e823fddbf7992d17445cca5dc3747d8
Jaime Tuyuc <jaimetuyuc@MBGTITO4214.local>
2022-10-01 21:23:49 -0600

    tailwind configuration

--- frontend/administrador-presupuesto/src/App.js
+++ frontend/administrador-presupuesto/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-red-500 flex-1 h-20 w-36">
       <h1>Budget Managment prueba gits</h1>
     </div>
   );
